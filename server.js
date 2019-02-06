eda4ca701d8b14756a04332ce389d5c4da0d6550
Kadin Zhang <cykadin@gmail.com>
2019-02-06 14:20:54 -0500

    force https using express

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + '/dist'));
-var port = process.env.PORT || 5000;
+const express = require('express');
+var secure = require('express-force-https');
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(secure);
+app.use(express.static(__dirname + '/dist/'));
+app.get(/.*/, (req, res) => {
+	res.sendfile(__dirname + '/dist/index.html');
+});
 app.listen(port);
-console.log('server started ' + port);
+
+console.log('Server started...');
